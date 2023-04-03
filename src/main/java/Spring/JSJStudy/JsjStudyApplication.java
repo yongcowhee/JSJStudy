0d2dc77e60fbf1be2d4894cff7b716d3662dd1f4
yongcowhee <yongcowhee@gmail.com>
2023-04-03 22:53:23 +0900

    Initial commit with Jinsung

---
+++ src/main/java/Spring/JSJStudy/JsjStudyApplication.java
@@ -0,0 +1,13 @@
+package Spring.JSJStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsjStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsjStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/JSJStudy/JsjStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.JSJStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsjStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
